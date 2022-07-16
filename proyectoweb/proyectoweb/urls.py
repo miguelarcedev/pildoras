564bd8ec9257f436d83367fd33c2abc458ab9a6d
Miguel Arce <correo.arce@gmail.com>
2022-07-16 14:44:11 -0300

    1

--- proyectoweb/proyectoweb/urls.py
+++ proyectoweb/proyectoweb/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from proyectowebapp import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="Home"),
-    path('servicio', views.servicios, name="Servicios"),
-    path('tienda', views.tienda, name="Tienda"),
-    path('blog', views.blog, name="Blog"),
-    path('contacto', views.contacto, name="Contacto"),
+    path('', include('proyectowebapp.urls')),
 
 ]
