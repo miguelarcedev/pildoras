6ef8600fdaecf308699df149109cda4be7d0fe59
miguelarcedev <correo.arce@gmail.com>
2022-09-02 09:38:17 -0300

    #

--- proyectoweb/proyectoweb/urls.py
+++ proyectoweb/proyectoweb/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('tienda/', include('tienda.urls')),
     path('carro/', include('carro.urls')),
-     path('autenticacion/', include('autenticacion.urls')),
+    path('autenticacion/', include('autenticacion.urls')),
 
 ]
